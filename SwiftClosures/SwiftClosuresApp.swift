44d4a35326097d5e8637f3b03c40e1033bda9be8
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-01 08:33:54 +0000

    Initial Commit

---
+++ SwiftClosures/SwiftClosuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftClosuresApp.swift
+//  SwiftClosures
+//
+//  Created by Tony Chaidinis on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftClosuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
