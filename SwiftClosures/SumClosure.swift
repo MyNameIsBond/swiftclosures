c6f79d045322768c2c18f8ff3dc44a935e3d5533
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-04 21:50:50 +0000

    uppercase

--- SwiftClosures/SumClosure.swift
+++ SwiftClosures/SumClosure.swift
@@ -9,13 +9,17 @@ let returnMax = { (a: [Int]) -> Int in
   return a.max { $0 < $1 }!
 }
 
+let upperCase = { (arr: [String]) -> () in
+  return arr.forEach({ $0.uppercased() })
+}
+
 struct SumClosure: View {
   @State var sum: Int = 0
   @State var myMax: Int = 0
   var body: some View {
     Text("Sum: \(sum)")
     Text("Sum: \(myMax)")
-    Button("Add"){
+    Button("Action"){
       sum = addClosure(2,3)
       myMax = returnMax([11,2,3,4,5])
     }
