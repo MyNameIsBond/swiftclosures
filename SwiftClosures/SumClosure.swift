d5d6a305b90f88d1b63a46b110d5799052b30170
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-04 21:24:34 +0000

    find max

--- SwiftClosures/SumClosure.swift
+++ SwiftClosures/SumClosure.swift
@@ -5,14 +5,25 @@ let addClosure = { (a: Int, b: Int) -> Int  in
   return a + b
 }
 
+let returnMax = { (a: [Int]) -> Int in
+  return a.max { $0 < $1 }!
+}
+
 struct SumClosure: View {
-    var body: some View {
-        Text("")
+  @State var sum: Int = 0
+  @State var myMax: Int = 0
+  var body: some View {
+    Text("Sum: \(sum)")
+    Text("Sum: \(myMax)")
+    Button("Add"){
+      sum = addClosure(2,3)
+      myMax = returnMax([11,2,3,4,5])
     }
+  }
 }
 
 struct SumClosure_Previews: PreviewProvider {
-    static var previews: some View {
-        SumClosure()
-    }
+  static var previews: some View {
+    SumClosure()
+  }
 }
