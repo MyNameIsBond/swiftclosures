5761b2e9b736e72927cda6bb57d1453f67cd6fdf
MyNameIsBond <hajdinim@uni.coventry.ac.uk>
2023-03-05 15:10:44 +0000

    first letter of each string to a closure

--- SwiftClosures/SumClosure.swift
+++ SwiftClosures/SumClosure.swift
@@ -17,21 +17,31 @@ let onlyEven = {(arr: [Int]) -> [Int] in
   return arr.filter{ ($0 % 2) == 0 }
 }
 
+let returnTheFirstLetter = {(first: String, second: String) -> String in
+  return "\(first.prefix(1)) --- \(second.prefix(1))"
+}
+
 struct SumClosure: View {
   @State var sum: Int = 0
   @State var myMax: Int = 0
   @State var myUppercase: [String] = ["This","is","Sparta"]
   @State var myOnlyEven: [Int] = [1,2,3,4,5,6]
+  @State var theFirstLetterOfStrings: String = "This"
   var body: some View {
     Text("Sum: \(sum)")
     Text("Max: \(myMax)")
     Button("Action"){
       sum = addClosure(2,3)
       myMax = returnMax([11,2,3,4,5])
+      
       myUppercase = upperCase(myUppercase)
-      myOnlyEven = onlyEven(myOnlyEven)
       print("This is all upperCase",myUppercase)
+      
+      myOnlyEven = onlyEven(myOnlyEven)
       print("only even", myOnlyEven)
+      
+      theFirstLetterOfStrings = returnTheFirstLetter(theFirstLetterOfStrings, theFirstLetterOfStrings)
+      print("first Chars", theFirstLetterOfStrings)
     }
   }
 }
